c41d062c2064809a1855736051908ecca2579632
Amit Kumar Sharma <amit.ops2020@gmail.com>
2025-10-01 13:30:39 +0530

    Update Adder_2.v

--- Verilog_Language/Modules:Hierarchy/Adder_2.v
+++ Verilog_Language/Modules:Hierarchy/Adder_2.v
@@ -1,20 +1,19 @@
-module top_module (
-    input [31:0] a,
-    input [31:0] b,
-    output [31:0] sum
-);
+module top_module (input [31:0] a, input [31:0] b, output [31:0] sum);
+    
     wire cout0;
-    wire [15:0] sum0,sum1;
-    add16 inst0(a[15:0],b[15:0],1'b0,sum0,cout0);
-    add16 inst1(a[31:16],b[31:16],cout0,sum1);
-    assign sum={sum1,sum0};
+    wire [15:0] sum0, sum1;
+    
+    add16 inst0(a[15:0], b[15:0], 1'b0, sum0, cout0);
+    add16 inst1(a[31:16], b[31:16], cout0, sum1);
+    
+    assign sum = {sum1, sum0};
 
 endmodule
 
-module add1 ( input a, input b, input cin,   output sum, output cout );
+module add1 (input a, input b, input cin, output sum, output cout);
 
-    assign sum={a^b}^cin;
-    assign cout={a^b}&cin|{a&b};
+    assign sum = {a ^ b} ^ cin;
+    assign cout = {a ^ b} & cin | {a & b};
 
 endmodule
 
