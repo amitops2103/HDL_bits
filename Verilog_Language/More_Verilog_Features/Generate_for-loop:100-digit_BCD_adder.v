c9ebfebcfe2a121cb980608c97e2cb41e279c481
Amit Kumar Sharma <amit.ops2020@gmail.com>
2025-10-24 00:10:54 +0530

    Update Generate_for-loop:100-digit_BCD_adder.v

--- Verilog_Language/More_Verilog_Features/Generate_for-loop:100-digit_BCD_adder.v
+++ Verilog_Language/More_Verilog_Features/Generate_for-loop:100-digit_BCD_adder.v
@@ -1,8 +1,4 @@
-module top_module( 
-    input [399:0] a, b,
-    input cin,
-    output cout,
-    output [399:0] sum );
+module top_module( input [399:0] a, b, input cin, output cout, output [399:0] sum);
     
     wire [99:0] cout_temp;
     
@@ -10,11 +6,12 @@ module top_module(
     
     genvar i;
     generate
-        for(i=1;i<100;i++) begin : bcd_add
+        for(i = 1;i < 100;i++) begin : bcd_add
             bcd_fadd insti(a[i*4+3:i*4],b[i*4+3:i*4],cout_temp[i-1],cout_temp[i],sum[i*4+3:i*4]);
         end
     endgenerate
-    assign cout=cout_temp[99];
+    
+    assign cout = cout_temp[99];
 
 endmodule
 
